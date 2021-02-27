818e5e0bf7cf62d2f1e874cf0a21ace460b3e5f6
punkadeedle <kn0517@gmail.com>
2021-02-26 21:33:20 -0600

    Update website content

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,14 @@
-console.log("Connected");
+new WOW().init();
+
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $("#mainNav");
+    $nav.toggleClass("scrolled", $(this).scrollTop() >$nav.height());
+
+  });
+});
+
+$("#toggler").click(function() {
+  var $nav = $("#mainNav");
+  $nav.toggleClass("scrolled");
+});
